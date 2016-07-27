fce3a4228970b05fe3af0e470e3d47e32699bf25
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-26 21:02:29 -0700

    make final edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/first_contact', to: 'contacts#first_contact'
+  get '/', to: 'contacts#index'
   get '/contacts', to: 'contacts#index'
   get '/phones', to: 'contacts#phone_index'
   get '/contacts/:id', to: 'contacts#show'
